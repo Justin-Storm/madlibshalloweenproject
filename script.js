dc22529806dc83e062a09914ca7c9b404ee5852d
Justin-Storm <144840724+Justin-Storm@users.noreply.github.com>
2024-10-23 14:52:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let manName = prompt('Enter a man\'s name.');
 let item1 = prompt('Enter an item.');
 let item2 = prompt('Enter another item.');
 
+adjective1.classList.add('highlight');
+adjective2.classList.add('highlight');
+manName.classList.add('highlight');
+item1.classList.add('highlight');
+item2.classList.add('highlight');
+
 let story = `It was a ${adjective1} and ${adjective2} night. It was Halloween and all the kids were going out to trick or treat while ${manName} was in his home. He grabbed a ${item1} and ${item2} and went out to scare the kids.`;
 
 let output = story;
